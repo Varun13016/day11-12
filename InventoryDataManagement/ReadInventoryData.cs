f9ba543e975675f756e91bec50af73a6c0326293
“Varun13016” <dmevarun@gmail.com>
2021-11-15 23:02:12 +0530

    InventoryDataManagement

---
+++ InventoryDataManagement/ReadInventoryData.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+
+namespace InventoryDataManagement
+{
+   public class ReadInventoryData
+    {
+        public InventoryDataManagementModel Read(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<InventoryDataManagementModel>(json);
+                }catch(Exception e)
+                {
+                    Console.WriteLine("problem in getting data {0}", e);
+                    return null;
+                }
+            }
+        }
+    }
+}
